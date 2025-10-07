1836849847f93ad3b84776540cac643b01130924
srirakshaprabhu <sriraksha.22cs160@sode-edu.in>
2025-10-07 11:55:45 +0530

    Add files via upload

---
+++ cie3a.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char* argv[]) {
+    int rank;
+    char message[50];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        MPI_Recv(message, 50, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 0 received message: %s\n", message);
+    } else if (rank == 1) {
+        MPI_Recv(message, 50, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 1 received message: %s\n", message);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
