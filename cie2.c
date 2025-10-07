464a37ad2189189bc18ddf46e5dba88f74a5082a
srirakshaprabhu <sriraksha.22cs160@sode-edu.in>
2025-10-07 11:53:12 +0530

    DD files via upload

---
+++ cie2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char* argv[]) {
+    int rank, size;
+    int number;
+
+    MPI_Init(&argc, &argv);               // Initialize MPI
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Get current process rank
+    MPI_Comm_size(MPI_COMM_WORLD, &size); // Get total number of processes
+
+    if (size < 2) {
+        if (rank == 0)
+            printf("This program requires at least 2 processes.\n");
+        MPI_Finalize();
+        return 0;
+    }
+
+    if (rank == 0) {
+        number = 5; // Sample input
+        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        printf("Process 0 sent integer: %d\n", number);
+    } else if (rank == 1) {
+        MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 1 received %d\n", number);
+        printf("Process 1 multiplied value = %d\n", number * 2);
+    }
+
+    MPI_Finalize(); // Finalize MPI
+    return 0;
+}
